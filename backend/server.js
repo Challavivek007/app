60e5b7efc7bed5b1c0959cf864eebe87f194c45e
Challavivek007 <challavivekreddy24@gmail.com>
2025-07-19 14:29:46 +0530

    changed app

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://aletisiddhureddy:o2JY0wR6sDBvlsNw@cluster0.wy5pyhr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+    await mongoose.connect("mongodb+srv://challavivekreddy24:MB8tBukAUqvDWvG8@cbox.ewynjpf.mongodb.net/?retryWrites=true&w=majority&appName=cbox", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       serverSelectionTimeoutMS: 5000,
